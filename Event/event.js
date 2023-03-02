0d65427e0950d418ee8601deae062da6fb793a3f
chami <chamikararuchiranga1@gmail.com>
2023-03-02 22:09:19 +0530

    js events

---
+++ Event/event.js
@@ -0,0 +1,19 @@
+function clickTownEvent () {
+    document.getElementById('clickTown').innerHTML = "Panadura";
+}
+
+function onChange (value) {
+    document.getElementById('onChangeLabel').innerHTML = value;
+}
+
+function onMouseOver () {
+    document.getElementById('mouseOver').style.backgroundColor = '#e85555';
+}
+
+function onMouseOut () {
+    document.getElementById('mouseOver').style.backgroundColor = '#b9b7b7';
+}
+
+function onKeyUp (value) {
+    document.getElementById('onKeyPress').innerHTML = value;
+}
